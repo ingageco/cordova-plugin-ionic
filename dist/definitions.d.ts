f5c99b69da95df77c5c84726d279d19465967888
Jay Hughes <jay@ingage.co>
2021-03-15 11:32:09 -0400

    dist

---
+++ dist/definitions.d.ts
@@ -0,0 +1,31 @@
+import { ICurrentConfig } from './IonicCordova';
+export interface IAvailableUpdate {
+    binaryVersionName: string;
+    binaryVersionCode: string;
+    clFrameworkVersion: string;
+    channel: string;
+    lastUsed: string;
+    state: string;
+    url: string;
+    versionId: string;
+    buildId: string;
+}
+export interface ISavedPreferences extends ICurrentConfig {
+    availableUpdate?: IAvailableUpdate;
+    updates: {
+        [versionId: string]: IAvailableUpdate;
+    };
+}
+export interface UpdateInfo {
+    versionId: string;
+    path: string;
+}
+export interface ManifestFileEntry {
+    integrity: string;
+    href: string;
+    size: number;
+}
+export interface FetchManifestResp {
+    manifestJson: ManifestFileEntry[];
+    fileBaseUrl: string;
+}
